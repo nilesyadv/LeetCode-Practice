2c780ae26167e272ad277845c132e9bfd1bbf70a
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-01-02 03:42:44 +0530

    Time: 12 ms (22.05%), Space: 13.5 MB (83.81%) - LeetHub

--- find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,47 +4,61 @@ public:
        
         int start=0,end=nums.size()-1;
         int mid=start+(end-start)/2;
-        float f=target-0.5,l=target+0.5;
-        int fo=0,lo=nums.size();
+        int fo=-1,lo=-1;
+    
         vector<int>v;
-       
         while(start<=end)
         {
-            if(nums[mid]>f)
+            if(nums[mid]>target)
             {
                 end=mid-1;
                 mid=start+(end-start)/2;
             }
-            else 
+            else if(nums[mid]<target)
             {
                 start=mid+1;
                 mid=start+(end-start)/2;
             }
+            else
+            {
+                fo=mid;
+                end=mid-1;
+                mid=start+(end-start)/2;
+            }
         }
-        fo=mid;
-        
-        start=0,end=nums.size()-1;
+      
+        end=nums.size()-1;
         mid=start+(end-start)/2;
         while(start<=end)
         {
-            if(nums[mid]>l)
+            if(nums[mid]>target)
             {
                 end=mid-1;
                 mid=start+(end-start)/2;
             }
-            else 
+            else if(nums[mid]<target)
             {
                 start=mid+1;
                 mid=start+(end-start)/2;
             }
+            else
+            {
+                lo=mid;
+                start=mid+1;
+                mid=start+(end-start)/2;
+            }
         }
-        lo=mid;
         
-        if(fo<nums.size() && nums[fo]==target) v.push_back(fo);
-        else v.push_back(-1);
+        v.push_back(fo);
+        v.push_back(lo);
+        //return v;
+    
         
-        if((lo-1)<nums.size() && nums[lo-1]==target) v.push_back(lo-1);
-        else v.push_back(-1);
+//         if(fo<nums.size() && nums[fo]==target) v.push_back(fo);
+//         else v.push_back(-1);
+        
+//         if((lo-1)<nums.size() && nums[lo-1]==target) v.push_back(lo-1);
+//         else v.push_back(-1);
         
         return v;
     }
