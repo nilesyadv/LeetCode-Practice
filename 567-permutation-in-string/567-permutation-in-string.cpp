1742ce065a8277d9f0d41ecf7bcd3d1f2d2a91cd
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-01-24 20:56:25 +0530

    Time: 8 ms (76.09%), Space: 7.4 MB (39.97%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        unordered_map<char,int>m1;
-        for(int i=0;i<s1.size();i++) m1[s1[i]]++;
-        //bool flag=false;
+        vector<int> v1(26),v2(26);
         if(s2.size()<s1.size()) return false;
-        for(int i=0;i<=s2.size()-s1.size();i++)
+        
+        for(int i=0;i<s1.size();i++) v1[s1[i]-'a']++;
+      
+        for(int i=0;i<s1.size();i++) v2[s2[i]-'a']++;
+        if(v1==v2) return true;
+        for(int i=s1.size();i<s2.size();i++)
         {
-            unordered_map<char,int>m2;
-            for(int j=i;j<i+s1.size();j++) m2[s2[j]]++;
-            bool flag=true;;
-            for(int t=0;t<s1.size();t++)
-            {
-                if(m1[s1[t]]!=m2[s1[t]])
-                {
-                    flag=false;
-                    break;
-                }
-            }
-            if(flag) return true;  
+            v2[s2[i-s1.size()]-'a']--;
+            v2[s2[i]-'a']++;
+            if(v1==v2) return true;
         }
         return false;
     }
