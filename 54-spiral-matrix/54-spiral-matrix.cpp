6b7f75f6b4f9edb9427ab826c05d616ec01e004a
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-01-26 18:17:15 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (71.72%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.cpp
+++ 54-spiral-matrix/54-spiral-matrix.cpp
@@ -3,63 +3,37 @@ public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int row=matrix.size();
         int col=matrix[0].size();
-        int a[10][10]={0};
         vector<int>ans;
-//         for(int i=0;i<row;i++)
-//         {
-//             for(int j=0;j<col;j++)
-//             {
-//                 cout<<arr[i][j]<<" ";
-                
-//             }
-//             cout<<endl;
-//         }
-        //cout<<row<<" "<<col<<endl;
+
         int sr=0,ec=col-1,er=row-1,sc=0;
         int cnt=0;
         while(cnt<row*col)
         {
-            for(int i=0;i<col;i++)
+            for(int i=sc;cnt<row*col && i<=ec;i++)
             {
-                if(a[sr][i]==0)
-                {
                     ans.push_back(matrix[sr][i]);
                     cnt++;
-                    a[sr][i]=1;
-                }
             }
             sr=sr+1;
             
-            for(int i=0;i<row;i++)
+            for(int i=sr;cnt<row*col && i<=er;i++)
             {
-                if(a[i][ec]==0)
-                {
                     ans.push_back(matrix[i][ec]);
-                    cnt++;
-                    a[i][ec]=1;
-                }
+                    cnt++;  
             }
             ec=ec-1;
             
-            for(int i=col-1;i>=0;i--)
+            for(int i=ec;cnt<row*col && i>=sc;i--)
             {
-                if(a[er][i]==0)
-                {
-                    ans.push_back(matrix[er][i]);
+               ans.push_back(matrix[er][i]);
                     cnt++;
-                    a[er][i]=1;
-                }
             }
             er=er-1;
             
-            for(int i=row-1;i>=0;i--)
+            for(int i=er;cnt<row*col && i>=sr;i--)
             {
-                if(a[i][sc]==0)
-                {
                     ans.push_back(matrix[i][sc]);
-                    cnt++;
-                    a[i][sc]=1;
-                }
+                    cnt++;         
             }
             sc=sc+1;
             
