f7cf54a2e38fafd2704ae861f1bec755d879c54b
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-01-27 13:12:56 +0530

    Time: 272 ms (48.33%), Space: 27.8 MB (19.58%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,20 +1,23 @@
 
+
+        
 class Solution {
 public:
     int countPrimes(int n) {
-        if(n<=2) return 0;
+    
         int a[n+1];
         for(int i=0;i<=n;i++) a[i]=1;
-        a[0]=0;
-        a[1]=0;
-        a[2]=1;
-        for(int i=2;i*i<=n;i++)
-        {
-            if(a[i]) for(int j=i;j*i<n;j++) a[j*i]=0;
-        }
         int cnt=0;
-        for(int i=0;i<n;i++) if(a[i]==1) cnt++;
+        for(int i=2;i<n;i++)
+        {
+            if(a[i]) 
+            {
+                cnt++;
+                for(int j=i;j<n;j=j+i) a[j]=0;
+            }
+        }
         return cnt;
   
     }
 };
+   
