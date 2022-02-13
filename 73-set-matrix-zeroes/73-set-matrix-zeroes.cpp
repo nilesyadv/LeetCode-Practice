86dd2e2653b0ca184936a0b0d04e803e1a2f9fa1
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-02-14 01:37:45 +0530

    Time: 24 ms (35.91%), Space: 13.3 MB (54.39%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,39 +3,49 @@ class Solution
 public:
     void setZeroes(vector<vector<int>> &matrix)
     {
-        int r=matrix.size();
-        int c=matrix[0].size();
-        bool row=false,col=false;
-        for(int i=0;i<r;i++)
+        int row = matrix.size();
+        int column = matrix[0].size();
+        bool r=0,c=0;
+        for(int i=0;i<row;i++)
         {
-            for(int j=0;j<c;j++)
+            for(int j=0;j<column;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    if(i==0) col=true;
-                    if(j==0) row=true;
+                    if(i==0) c=1;
+                    if(j==0) r=1;
                     matrix[i][0]=0;
                     matrix[0][j]=0;
                 }
             }
         }
-        
-        for(int i=1;i<r;i++)
+           
+        for(int i=1;i<row;i++)
         {
-            for(int j=1;j<c;j++)
+            if(matrix[i][0]==0)
             {
-                if(matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j]=0;
+                for(int j=0;j<column;j++)
+                {
+                    matrix[i][j]=0;
+                }
             }
         }
-        //cout<<col;
-        if(row) 
+        
+        for(int i=1;i<column;i++)
         {
-            for(int i=0;i<r;i++) matrix[i][0]=0;
+            if(matrix[0][i]==0)
+            {
+                for(int j=0;j<row;j++)
+                {
+                    matrix[j][i]=0;
+                }
+            }
         }
         
-        if(col) 
-        {
-            for(int i=0;i<c;i++) matrix[0][i]=0;
-        }
+        
+       // cout<<r<<c;
+       if(r) for(int i=0;i<row;i++) matrix[i][0]=0;
+       if(c) for(int j=0;j<column;j++) matrix[0][j]=0;
+       
     }
 };
