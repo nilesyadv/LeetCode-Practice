89d8db2df1993990ed83e6eb0c5c9b8716d7e4ae
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-02-25 19:13:06 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (35.61%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.cpp
+++ 165-compare-version-numbers/165-compare-version-numbers.cpp
@@ -9,41 +9,27 @@ public:
         if(cnt1<cnt2) for(int i=0;i<k;i++) version1+=".0";
         else for(int i=0;i<k;i++) version2+=".0";
         
-        //cout<<version1<<endl<<version2<<endl;
         int i=0,j=0;
         while(1)
         {
             string s1;
             while(i<version1.size() && version1[i]!='.')
-            {
-                s1+=version1[i];
-                //num1=num1+k*(int)(version1.at(i)-'0');
-                i++;
-                //if(i>=0 && version1[i]!='.') k*=10;
-            }
+                s1+=version1[i++];
             i++;
+            
             int num1=stoi(s1);
             
             string s2;
             while(j<version2.size() && version2[j]!='.')
-            {
-                s2+=version2[j]; 
-                //num2=num2+k1*(int)(version2.at(j)-'0');
-                 j++;
-                 //if(j>=0 && version2[j]!='.') k1*=10;
-            }
+                s2+=version2[j++];
             j++;
-            int num2=stoi(s2);
-            //cout<<num1<<" "<<num2<<" "<<i<<" "<<j<<endl;
-            //if(num1!=num2) nums1=num1,nums2=num2;
             
+            int num2=stoi(s2);
+           
             if(num1<num2) return -1;
             else if(num1>num2) return 1;
             if(i>=version1.size() && j>=version2.size()) break;
         }
-        // if(nums1>nums2) return 1;
-        // else if(nums1<nums2) return -1;
         return 0;
-  
     }
 };
