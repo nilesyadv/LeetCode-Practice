b6e4bb39512de0d0b325e67b19407463265dc005
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-04-13 18:30:24 +0530

    Time: 16 ms (69.86%), Space: 9.7 MB (33.37%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -4,6 +4,7 @@ public:
         int max_len=0;
         int l=0,r=0;
         unordered_set<char> set;
+        unordered_map <char,int> m;
         int n=s.size();
         while(r<n)
         {
@@ -11,12 +12,22 @@ public:
             {
                 max_len=max(max_len,r-l+1);
                 set.insert(s[r]);
+                m[s[r]]=r;
                 r++;
             }
             else
             {
-                set.erase(s[l]);
-                l++;
+                if(m[s[r]]<l) 
+                {
+                    m[s[r]]=r;
+                }
+                else
+                {
+                    l=m[s[r]]+1;
+                    m[s[r]]=r;
+                }
+                max_len=max(max_len,r-l+1);
+                r++;  
             }
         }
         
