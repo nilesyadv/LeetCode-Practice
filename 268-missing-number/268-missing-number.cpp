02e4284d08d6df32ce94cdd597be7a02e5bbcc3d
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-05-28 15:42:38 +0530

    Time: 20 ms (80.22%), Space: 17.9 MB (93.84%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        long long sum=0;
+        
         int n=nums.size();
-        long long tobesum=(n*(n+1))/2;
-        for(int i=0;i<n;i++) sum+=nums[i];
-        return tobesum-sum;
+        int ans=0;
+        for(int i=1;i<=n;i++)
+        {
+            ans=ans^nums[i-1];
+            ans=ans^i;
+        }
+        return ans;
     }
 };
