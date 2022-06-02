e04e883519fdafee68dca5e7c7794223636bccbc
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-03 01:49:37 +0530

    Time: 6 ms (29.07%), Space: 5.9 MB (74.74%) - LeetHub

---
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        long long n=dividend,m=divisor;
+        long long temp=0,q=0;
+        int sign=1;
+        if((n>=0 && m<0) || (n<0 && m>=0)) sign=-1;
+        n=abs(n);
+        m=abs(m);
+        if(m>n) return 0;
+        if(m==n) return 1*sign;
+        //cout<<n<<" "<<m<<endl;
+        
+        for(long long i=32;i>=0;i--)
+        {
+            if(temp+(m<<i)<=n)
+            {
+                temp+=(m<<i);
+                q|=(1LL<<i);
+                //cout<<i<<" "<<temp<<" "<<q<<endl;
+            }
+        }
+        
+        //cout<<q<<endl;
+        
+        if(sign==-1) q=q*-1;
+        if(q>INT_MAX) return INT_MAX;
+        else if(q<INT_MIN) return INT_MIN;
+        else return q;
+    }
+};
