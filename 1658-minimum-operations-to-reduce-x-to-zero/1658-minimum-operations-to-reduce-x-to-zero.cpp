33a82956e320697d6afde5ac0824929ab622f739
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-13 17:15:11 +0530

    Time: 1066 ms (6.09%), Space: 204.2 MB (5.01%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        map<int,pair<int,int>> m;
+        unordered_map<int,pair<int,int>> m;
         int sum=0,n=nums.size();
         for(int i=n-1;i>=0;i--)
         {
             sum+=nums[i];
             m[sum]={1,i};
-            //cout<<sum<<" "<<m[sum].first<<" "<<m[sum].second<<endl;
         }
-        //cout<<endl;
         
         if(x<nums[0] && x<nums[n-1]) return -1;
         if(sum==x) return n;
         
         int ans=1000000;
-        // cout<<sum<<" "<<m[sum].first<<" "<<m[sum].second<<endl;
-        // cout<<endl;
         if(m[x].first==1) ans=min(ans,n-m[x].second);
         sum=0;
+        
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
@@ -31,14 +28,9 @@ public:
             {
                 int y=x-sum;
                 if(m[y].first==1 && m[y].second>i)
-                {
                     ans=min(ans,i+1+n-m[y].second);
-                    //cout<<y<<" "<<i<<" "<<m[y].first<<" "<<m[y].second<<endl;
-                }
-            }
-           
+            }  
         }
-        
         
         if(ans==1000000) return -1;
         else return ans;
