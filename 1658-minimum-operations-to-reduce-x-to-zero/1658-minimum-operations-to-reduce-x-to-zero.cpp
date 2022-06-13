6a981bae09ecd8caced4db3f8a6f9df7bf119144
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-13 17:40:11 +0530

    Time: 174 ms (96.15%), Space: 98.6 MB (60.71%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,38 +1,51 @@
+#define sp " "
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        unordered_map<int,pair<int,int>> m;
+        
         int sum=0,n=nums.size();
-        for(int i=n-1;i>=0;i--)
+        for(int i=0;i<n;i++) sum+=nums[i];
+        int target=sum-x,i=0,curr_sum=0,maxlength=0;
+        bool found=false;
+        for(int j=0;j<n;j++)
         {
-            sum+=nums[i];
-            m[sum]={1,i};
-        }
-        
-        if(x<nums[0] && x<nums[n-1]) return -1;
-        if(sum==x) return n;
-        
-        int ans=1000000;
-        if(m[x].first==1) ans=min(ans,n-m[x].second);
-        sum=0;
-        
-        for(int i=0;i<n;i++)
-        {
-            sum+=nums[i];
-            if(sum>x) break;
-            if(sum==x)
+            curr_sum+=nums[j];
+            while(curr_sum>target && i<=j) 
             {
-                ans=min(ans,i+1);
+                curr_sum-=nums[i];
+                i++;
             }
-            else
+            
+            if(curr_sum==target)
             {
-                int y=x-sum;
-                if(m[y].first==1 && m[y].second>i)
-                    ans=min(ans,i+1+n-m[y].second);
-            }  
+                maxlength=max(maxlength,j-i+1);class Solution {
+   public:
+    int minOperations(vector<int>& nums, int x) {
+        int sum = 0, n = nums.size();
+        for (int i : nums) sum += i;
+        int target = sum - x;
+        int curr_sum = 0, max_len = 0;
+        int start_idx = 0;
+        bool found = false;
+        for (int end_idx = 0; end_idx < n; end_idx++) {
+            curr_sum += nums[end_idx];
+            while (start_idx <= end_idx && curr_sum > target) {
+                curr_sum -= nums[start_idx];
+                start_idx += 1;
+            }
+            if (curr_sum == target) {
+                found = true;
+                max_len = max(max_len, end_idx - start_idx + 1);
+            }
+        }
+        return found ? n - max_len : -1;
+    }
+};
+                found=true;
+            }
         }
         
-        if(ans==1000000) return -1;
-        else return ans;
+        if(found) return n-maxlength;
+        else return -1;
     }
 };
