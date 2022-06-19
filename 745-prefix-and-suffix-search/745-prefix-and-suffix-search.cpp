06a645324bcfafc28d8d86d0cd0e0dbbdf866434
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-19 21:00:12 +0530

    Time: 1327 ms (6.02%), Space: 154 MB (59.78%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.cpp
@@ -1,25 +1,39 @@
 class WordFilter {
-   private:
-    unordered_map<string, int> hashMap;
+    unordered_map<string,int> map;
 
    public:
     WordFilter(vector<string>& words) {
-        int n = words.size();
-        for (int i = 0; i < n; i++) {
-            string word = words[i];
-            int wordSize = word.size();
-            for (int j = 1; j <= wordSize; j++) {
-                string p = word.substr(0, j);
-                for (int k = 0; k < wordSize; k++) {
-                    string s = word.substr(k, wordSize);
-                    hashMap[p + "|" + s] = i + 1;
+        int n=words.size();
+        for(int i=0;i<n;i++)
+        {
+            int wordSize=words[i].size();
+            for(int j=0;j<wordSize;j++)
+            {
+                string p=words[i].substr(0,j+1);
+                int m=p.size();
+                for(int k=0;k<wordSize;k++)
+                {
+                    string s=words[i].substr(k);
+                    string t=p+'|'+s;
+                  //  cout<<t<<" ";
+                    map[t]=i+1;
                 }
             }
+            
+          //  cout<<endl;
         }
     }
 
     int f(string prefix, string suffix) {
-        string s = prefix + "|" + suffix;
-        return hashMap[s] - 1;
+        string t=prefix+'|'+suffix;
+        // cout<<endl;
+        // cout<<t<<endl;
+        //cout<<map[t]<<endl;
+        if(map[t]==0) return -1;
+        else return map[t]-1;
     }
 };
+
+// // ["WordFilter","f","f","f","f","f","f","f","f","f","f"]
+// // [[["cabaabaaaa","ccbcababac","bacaabccba","bcbbcbacaa","abcaccbcaa","accabaccaa","cabcbbbcca","ababccabcb","caccbbcbab","bccbacbcba"]]
+// ,["bccbacbcba","a"],["ab","abcaccbcaa"],["a","aa"],["cabaaba","abaaaa"],["cacc","accbbcbab"],["ccbcab","bac"],["bac","cba"],["ac","accabaccaa"],["bcbb","aa"],["ccbca","cbcababac"]]
