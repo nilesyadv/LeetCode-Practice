bc5f9c393e768430bc010dbf859626b14e73ecff
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-25 13:52:08 +0530

    Time: 49 ms (27.45%), Space: 27.1 MB (53.59%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -2,29 +2,19 @@ class Solution {
 public:
     bool checkPossibility(vector<int>& nums) {
         int needToDecrease=0,needToIncrease=0;
-        vector<int> v; 
-        v=nums;
         int size=nums.size();
+        int lastElement=nums[size-1],firstElement=nums[0];
         for(int i=size-2;i>=0;i--)
         {
-            if(nums[i]>nums[i+1])
-            {
-                nums[i]=nums[i+1];
-                needToDecrease++;
-            }
+            if(nums[i]>lastElement) needToDecrease++;
+            else lastElement=nums[i];
         }
-        nums=v;
-        
+       
         for(int i=1;i<size;i++)
         {
-            if(nums[i]<nums[i-1])
-            {
-                nums[i]=nums[i-1];
-                needToIncrease++;
-            }
+            if(nums[i]<firstElement) needToIncrease++;
+            else firstElement=nums[i];
         }
-        
-        //cout<<needToIncrease<<" "<<needToDecrease<<endl;
         
         if(needToIncrease>1 && needToDecrease>1) return false;
         return true;
