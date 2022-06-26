d534e5179336288c3bdc3125331d1a83b7edbbd7
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-26 23:34:04 +0530

    Time: 113 ms (22.35%), Space: 42.5 MB (48.14%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -4,26 +4,20 @@ public:
         long long sum=0;
         int n=cardPoints.size();
         for(int i=0;i<n;i++) sum+=cardPoints[i];
-        //cout<<sum<<endl;
+        
         if(n-k==0) return sum;
         int i=0,j=(i+n-k-1);
         long long leastSum=0;
-        //cout<<i<<" "<<j<<endl;
+        
         for(int k=0;k<=j;k++) leastSum+=cardPoints[k];
         i++; j++;
-        //cout<<leastSum<<" "<<endl;
         long long currSum=leastSum;
         while(j<n)
         {
-            
             currSum=currSum-cardPoints[i-1]+cardPoints[j];
             leastSum=min(leastSum,currSum);
-            
-           // cout<<leastSum<<" "<<i<<" "<<j<<endl;;
-            i++;
-            j++;
+            i++; j++;
         }
-        //cout<<endl;
         return sum-leastSum;
     } 
 };
