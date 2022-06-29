882595d3563323b07a72731d6600519f001fe3d8
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-06-29 18:49:16 +0530

    Time: 307 ms (14.15%), Space: 82.6 MB (66.92%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.cpp
@@ -3,12 +3,9 @@ public:
     int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
         
         int n=nums1.size();
-        vector<int> v1,v2;
         int totalSum1=0,totalSum2=0;
-        
-       // for(int i=0;i<n;i++) {v1.push_back(nums2[i]-nums1[i]); v2.push_back(nums2[i]-nums1[i]);}
-        
         int maxDelta1=0,maxDelta2=0,curr1=0,curr2=0;
+        
         for(int i=0;i<n;i++)
         {
             curr1+=nums2[i]-nums1[i];
