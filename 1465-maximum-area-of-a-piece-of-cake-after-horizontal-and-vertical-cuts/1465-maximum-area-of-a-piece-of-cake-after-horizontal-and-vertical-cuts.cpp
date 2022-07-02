c51aa6c2c35b37ea9aba76dabb9bfa51b8b46e78
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-03 00:28:19 +0530

    Time: 78 ms (93.66%), Space: 32.3 MB (40.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -2,14 +2,14 @@ class Solution {
 public:
     int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
     
+	    horizontalCuts.push_back(0); horizontalCuts.push_back(h);
+		verticalCuts.push_back(0); verticalCuts.push_back(w);
+		
         sort(horizontalCuts.begin(),horizontalCuts.end());
         sort(verticalCuts.begin(),verticalCuts.end());
-        int maxRowDiff=horizontalCuts[0]-0;
-        int maxColDiff=verticalCuts[0]-0;
         
-        if(horizontalCuts.back()!=h) horizontalCuts.push_back(h);
-        if(verticalCuts.back()!=w) verticalCuts.push_back(w);
         int row=horizontalCuts.size(),col=verticalCuts.size();
+		int maxRowDiff=0, maxColDiff=0;
         
         for(int i=1;i<row;i++) maxRowDiff=max(maxRowDiff,horizontalCuts[i]-horizontalCuts[i-1]);
         for(int i=1;i<col;i++) maxColDiff=max(maxColDiff,verticalCuts[i]-verticalCuts[i-1]);
@@ -17,7 +17,6 @@ public:
         long long MOD=1e9+7;
         long long ans=((maxRowDiff%MOD)*(maxColDiff%MOD))%MOD;
         return ans;
-        
-        
+              
     }
 };
