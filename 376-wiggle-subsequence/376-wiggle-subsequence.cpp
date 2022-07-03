68ccf5c606b750e4163a0dae5f3066ad2b0026ff
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-03 22:26:47 +0530

    Time: 65 ms (5.09%), Space: 7.5 MB (14.56%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,60 +1,41 @@
-class Solution {
+class Solution
+{
 public:
-    int wiggleMaxLength(vector<int>& nums) {
-        int n=nums.size();
-        vector<vector<int>> v(n,vector<int> (2,-1));
-        v[0][0]=1;
-        if(n==1) return 1;
-        if(n==2)
+    int wiggleMaxLength(vector<int> &nums)
+    {
+        int n = nums.size();
+        vector<vector<int>> v(n, vector<int>(2, -1));
+        v[0][0] = 1;
+
+        for (int i = 0; i < n; i++)
         {
-            if(nums[0]!=nums[1]) return 2;
-            else return 1;
-        }
-        
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<i;j++)
+            for (int j = 0; j < i; j++)
             {
-                //cout<<i<<"-"<<j<<"-"<<nums[j]<<"-"<<nums[i]<<"-"<<v[j][1]<<" ";
-                if(j==0)
+                if (v[j][1] == -1)
                 {
-                    if(nums[j]!=nums[i])
+                    if (nums[i] != nums[j])
                     {
-                        if(v[i][0]<1+v[j][0])
+                        if (v[i][0] < 1 + v[j][0])
                         {
-                            v[i][0]=v[j][0]+1;
-                            v[i][1]=j;
+                            v[i][0] = v[j][0] + 1;
+                            v[i][1] = j;
                         }
                     }
                 }
-                else
+                else if ((nums[j] > nums[v[j][1]] && nums[j] > nums[i]) || (nums[j] < nums[v[j][1]] && nums[j] < nums[i]))
                 {
-                    if(v[j][1]==-1) 
+                    if (v[i][0] < 1 + v[j][0])
                     {
-                        if(nums[i]!=nums[j])
-                        {
-                            if(v[i][0]<1+v[j][0])
-                            {
-                                v[i][0]=v[j][0]+1;
-                                v[i][1]=j;
-                            }
-                        }
-                    }
-                    else if((nums[j]>nums[v[j][1]] && nums[j]>nums[i]) || (nums[j]<nums[v[j][1]] && nums[j]<nums[i]))
-                    {
-                        if(v[i][0]<1+v[j][0])
-                        {
-                            v[i][0]=v[j][0]+1;
-                            v[i][1]=j;
-                        }
+                        v[i][0] = v[j][0] + 1;
+                        v[i][1] = j;
                     }
                 }
             }
-            //cout<<endl;
         }
-        int ans=1;
-        for(int i=0;i<n;i++) ans=max(v[i][0],ans);
+        
+        int ans = 1;
+        for (int i = 0; i < n; i++)
+            ans = max(v[i][0], ans);
         return ans;
-    
     }
- };
+};
