d97fe4f43d2337c5211e9725e14bde9fed135b0c
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-05 03:06:39 +0530

    Time: 48 ms (20.14%), Space: 17.8 MB (65.94%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n=ratings.size(),sum=0;
-        vector<pair<int,int>> v;
-        for(int i=0;i<n;i++) v.push_back({ratings[i],i});
-        sort(v.begin(),v.end());
+        
+    
         vector<int> candies(n,1);
-        for(int i=0;i<n;i++)
+        for(int i=1;i<n;i++)
+            if(ratings[i]>ratings[i-1]) candies[i]=candies[i-1]+1;
+        
+        sum+=candies[n-1];
+        for(int i=n-2;i>=0;i--)
         {
-            int currIndex=v[i].second;
-            int prevIndex=currIndex-1,nextIndex=currIndex+1;
-            if(prevIndex>=0 && ratings[currIndex]>ratings[prevIndex]) candies[currIndex]=max(candies[currIndex],candies[prevIndex]+1);
-            if(nextIndex<n && ratings[currIndex]>ratings[nextIndex]) candies[currIndex]=max(candies[currIndex],candies[nextIndex]+1);
-            sum+=candies[currIndex];
+            if(ratings[i]>ratings[i+1]) candies[i]=max(candies[i],candies[i+1]+1);
+            sum+=candies[i];
         }
         return sum;
     }
