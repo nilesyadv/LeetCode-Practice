11a5289fc77840ceda61ca7514f02e86ed85e801
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-10 03:16:22 +0530

    Time: 272 ms (64.01%), Space: 91 MB (43.77%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     int maxResult(vector<int>& nums, int k) {
         
-        set<pair<int,int>, greater<pair<int,int>>> s;
+        priority_queue<pair<int,int>> p;
         int n=nums.size();
         
         int dp[n]; dp[0]=nums[0];
-        s.insert({dp[0],0});
+        p.push({dp[0],0});
         
         for(int i=1;i<n;i++)
         {
-            if(s.size()>k) s.erase({dp[i-k-1],i-k-1});
-            
-            pair<int,int> p=*s.begin();
-            dp[i]=nums[i]+p.first; 
-            
-            s.insert({dp[i],i});
+            while(p.top().second<i-k) p.pop();
+            dp[i]=nums[i]+p.top().first; 
+            p.push({dp[i],i});
         }
         
         return dp[n-1];
