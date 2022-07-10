57ece3c47e30100cf28aa5f92d51c6c0e33225a3
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-11 03:37:01 +0530

    Time: 149 ms (39.34%), Space: 12.2 MB (35.47%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,58 +1,54 @@
-class Solution {
+class Solution
+{
     long long dp[105][25][105];
-    long long maxi=1000000000;
-    //long long ans=maxi;
-    long long dfs(int i,int prev,int k,vector<int>& houses, vector<vector<int>>& cost,int m, int n)
+    long long maxi = 1000000000;
+    
+    long long dfs(int i, int prev, int k, vector<int> &houses, vector<vector<int>> &cost, int m, int n)
     {
-        //cout<<i<<" "<<prev<<" "<<k<<endl;
-        if(k<0) return maxi;
-        //cout<<"H"<<i<<" "<<k<<endl;
-        //cout<<i<<" "<<prev<<" "<<k<<endl;
-        // if(k==0)
-        // {
-        //     return cost[i][prev];
-        // }
-        if(i>=m)
-        {
-            if(k==0) return 0;
-            else return maxi;
-        }
+    
+        if (k < 0)
+            return maxi;
         
-        if(dp[i][prev][k]!=-1) return dp[i][prev][k];
-        if(houses[i])
+        if (i >= m)
         {
-            if(houses[i]!=prev) k--;
-            return dfs(i+1,houses[i],k,houses,cost,m,n);
+            if (k == 0)
+                return 0;
+            else
+                return maxi;
         }
-        //cout<<"hey"<<endl;
-        long long ans=maxi;
-        long long temp=maxi;
 
-         for(int j=0;j<n;j++)
-         { 
-            if(j+1==prev) ans=min(ans,cost[i][j]+dfs(i+1,j+1,k,houses,cost,m,n));
-            else if(j+1!=prev) ans=min(ans,cost[i][j]+dfs(i+1,j+1,k-1,houses,cost,m,n));
-          }   
-       // cout<<ans<<endl;
-        
-        return dp[i][prev][k]=ans;
-    }
-public:
-    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        for(int i=0;i<105;i++)
+        if (dp[i][prev][k] != -1)
+            return dp[i][prev][k];
+            
+        if (houses[i])
         {
-            for(int j=0;j<25;j++)
-            {
-                for(int k=0;k<105;k++)
-                {
-                    dp[i][j][k]=-1;
-                }
-            }
+            if (houses[i] != prev)
+                k--;
+            return dfs(i + 1, houses[i], k, houses, cost, m, n);
         }
         
-        long long ans=dfs(0,0,target,houses,cost,m,n);
-        
-        
-        return ans==maxi?-1:ans;
+        long long ans = maxi;
+        for (int j = 0; j < n; j++)
+        {
+            if (j + 1 == prev)
+                ans = min(ans, cost[i][j] + dfs(i + 1, j + 1, k, houses, cost, m, n));
+            else if (j + 1 != prev)
+                ans = min(ans, cost[i][j] + dfs(i + 1, j + 1, k - 1, houses, cost, m, n));
+        }
+
+        return dp[i][prev][k] = ans;
+    }
+
+public:
+    int minCost(vector<int> &houses, vector<vector<int>> &cost, int m, int n, int target)
+    {
+
+        for (int i = 0; i < 105; i++)
+            for (int j = 0; j < 25; j++)
+                for (int k = 0; k < 105; k++)
+                    dp[i][j][k] = -1;
+
+        long long ans = dfs(0, 0, target, houses, cost, m, n);
+        return ans == maxi ? -1 : ans;
     }
 };
