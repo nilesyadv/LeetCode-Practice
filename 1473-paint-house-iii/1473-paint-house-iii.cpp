83d2da451f84b26bfd2b5a603f6273ae1ae207cb
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-11 03:34:50 +0530

    Time: 110 ms (67.79%), Space: 12.2 MB (35.47%) - LeetHub

--- 1473-paint-house-iii/1473-paint-house-iii.cpp
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -1,49 +1,58 @@
 class Solution {
+    long long dp[105][25][105];
+    long long maxi=1000000000;
+    //long long ans=maxi;
+    long long dfs(int i,int prev,int k,vector<int>& houses, vector<vector<int>>& cost,int m, int n)
+    {
+        //cout<<i<<" "<<prev<<" "<<k<<endl;
+        if(k<0) return maxi;
+        //cout<<"H"<<i<<" "<<k<<endl;
+        //cout<<i<<" "<<prev<<" "<<k<<endl;
+        // if(k==0)
+        // {
+        //     return cost[i][prev];
+        // }
+        if(i>=m)
+        {
+            if(k==0) return 0;
+            else return maxi;
+        }
+        
+        if(dp[i][prev][k]!=-1) return dp[i][prev][k];
+        if(houses[i])
+        {
+            if(houses[i]!=prev) k--;
+            return dfs(i+1,houses[i],k,houses,cost,m,n);
+        }
+        //cout<<"hey"<<endl;
+        long long ans=maxi;
+        long long temp=maxi;
+
+         for(int j=0;j<n;j++)
+         { 
+            if(j+1==prev) ans=min(ans,cost[i][j]+dfs(i+1,j+1,k,houses,cost,m,n));
+            else if(j+1!=prev) ans=min(ans,cost[i][j]+dfs(i+1,j+1,k-1,houses,cost,m,n));
+          }   
+       // cout<<ans<<endl;
+        
+        return dp[i][prev][k]=ans;
+    }
 public:
-    //declare dp of size m*m*n (the max possible)
-    int dp[100][100][21];
-    
-    const int mx = 1e7+1;
-    int f(vector<int>& houses, vector<vector<int>>& cost, int target, int idx, int count, int prev){
-        //base cases
-        if(idx == houses.size()) return count==target ? 0 : mx;
-        
-        //memoization
-        if(dp[idx][count][prev]!=-1) return dp[idx][count][prev];
-        
-        int minCost = mx;
-        //If already painted
-        if(houses[idx]){
-            
-            //if prev house is of different color, increase the count by 1
-            if(houses[idx]!=prev) minCost = f(houses,cost,target,idx+1,count+1,houses[idx]);
-            
-            else minCost = f(houses,cost,target,idx+1,count,houses[idx]);
-            
-        }else{
-            //we have to try all the colors
-            for(int j=0;j<cost[0].size();j++){
-                int tmp;  //store the current cost
-                
-                //if prev house is of different color, increase the count by 1
-                if((j+1)!=prev) tmp = cost[idx][j] + f(houses,cost,target,idx+1,count+1,j+1);
-                
-                else tmp = cost[idx][j] + f(houses,cost,target,idx+1,count,j+1);
-                
-                minCost = min(minCost,tmp);
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        for(int i=0;i<105;i++)
+        {
+            for(int j=0;j<25;j++)
+            {
+                for(int k=0;k<105;k++)
+                {
+                    dp[i][j][k]=-1;
+                }
             }
         }
-        //store before returning
-        return dp[idx][count][prev] = minCost;
-    }
-    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
-        memset(dp,-1,sizeof(dp));
         
-        // 0 0 0 represents the current index(idx) , neighbourhood count(count), color of prev house(prev)
-        int ans = f(houses,cost,target,0,0,0);
+        long long ans=dfs(0,0,target,houses,cost,m,n);
         
-        //if no solution exist
-        if(ans==mx) return -1;
-        else return ans;
+        
+        return ans==maxi?-1:ans;
     }
 };
