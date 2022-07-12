997bd73cb287c1b0fbd9b8f8fb969089eb8b32fa
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-12 05:54:11 +0530

    Time: 7 ms (46.26%), Space: 12 MB (44.65%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -10,27 +10,19 @@
  * };
  */
 class Solution {
+    void RRL(TreeNode* root, int currLevel, vector<int> &ans)
+    {
+        if(root==NULL) return;
+        
+        if(ans.size()==currLevel) ans.push_back(root->val);
+        RRL(root->right, currLevel+1, ans);
+        RRL(root->left, currLevel+1, ans);
+    }
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        queue<TreeNode*> q1;
-        if(root) q1.push(root);
-        
-        while(true)
-        {
-            if(q1.empty()) break;
-            queue<TreeNode*> q2;
-            while(!q1.empty()) {q2.push(q1.front()); q1.pop();}
-    
-            while(!q2.empty())
-            {
-                if(q2.front()->left) q1.push(q2.front()->left);
-                if(q2.front()->right) q1.push(q2.front()->right);
-                if(q2.size()==1) ans.push_back(q2.front()->val);
-                q2.pop();
-            }
-              
-        }
+        int cntLevel=0;
+        RRL(root,0,ans);
         return ans;
     }
 };
