83f60f0f155316f4a76117540604afa9ad59290d
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-19 06:53:55 +0530

    Time: 350 ms (5.01%), Space: 15.3 MB (28.43%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,30 +1,40 @@
 class Solution {
-public:
-    // int dp[102][102];
-    bool helper(string& s1, string& s2, string &s3, int l1, int l2, int l3, vector<vector<int>>&dp) {
-        if(l1<0 && l2<0 && l3<0) {
-            return true;
+    int dp[101][101][201];
+    bool Interleave(string &s1, string &s2, string &s3, int i, int j, int k)
+    {
+        //cout<<i<<" "<<j<<" "<<k<<endl;
+        //return true;
+        if(i==s1.size() && j==s2.size() && k==s3.size()) return dp[i][j][k]=1;
+        if(dp[i][j][k]!=-1) return dp[i][j][k];
+        if(i==s1.size())
+        {
+            if(s3[k]==s2[j]) return dp[i][j][k]=Interleave(s1,s2,s3,i,j+1,k+1);
+            else return dp[i][j][k]=0;
         }
-        if(l1>=0 && l2>=0 && dp[l1][l2]!=-1) {
-            return dp[l1][l2];
+        else if(j==s2.size())
+        {
+            if(s3[k]==s1[i]) return dp[i][j][k]=Interleave(s1,s2,s3,i+1,j,k+1);
+            else return dp[i][j][k]=0;
         }
-        if(l1>=0 && s1[l1]==s3[l3] && l2>=0 && s2[l2]==s3[l3] ) {
-            return dp[l1][l2] = (helper(s1, s2, s3, l1-1, l2, l3-1, dp) || helper(s1, s2, s3, l1, l2-1, l3-1, dp));
+        else
+        {
+            if(s3[k]==s1[i] && s3[k]==s2[j])
+                return dp[i][j][k]=(Interleave(s1,s2,s3,i+1,j,k+1) | Interleave(s1,s2,s3,i,j+1,k+1));
+            else if(s3[k]==s1[i]) return dp[i][j][k]=Interleave(s1,s2,s3,i+1,j,k+1);
+            else if(s3[k]==s2[j]) return dp[i][j][k]=Interleave(s1,s2,s3,i,j+1,k+1);
+            else return dp[i][j][k]=0;                                      
         }
-        else if(l1>=0 && s1[l1]==s3[l3])
-            return helper(s1, s2, s3, l1-1, l2, l3-1, dp);
-        
-        else if(l2>=0 &&s2[l2]==s3[l3])
-            return helper(s1, s2, s3, l1, l2-1, l3-1, dp);
-        
-        else return false;
     }
     
+public:
     bool isInterleave(string s1, string s2, string s3) {
-        if(s3.size() != s1.size() + s2.size())
-			return false;
-        vector<vector<int>>dp(s1.size()+1, (vector<int>(s2.size()+1, -1)));
-        // memset(dp, -1, sizeof(dp));
-        return helper(s1, s2, s3,s1.size()-1, s2.size()-1, s3.size()-1, dp);
+        if(s3.size()!=s1.size()+s2.size()) return false;
+        //int dp[101][101][201];
+        for(int i=0;i<=100;i++)
+            for(int j=0;j<=100;j++)
+                for(int k=0;k<=200;k++) dp[i][j][k]=-1;
+       
+        return Interleave(s1,s2,s3,0,0,0);
+        return true;
     }
 };
