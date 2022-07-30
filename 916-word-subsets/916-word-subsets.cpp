ba5aa2b16ff6b21c8c2152406ce1e18e44f943be
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-07-31 04:14:15 +0530

    Time: 253 ms (70.70%), Space: 102.4 MB (40.31%) - LeetHub

--- 916-word-subsets/916-word-subsets.cpp
+++ 916-word-subsets/916-word-subsets.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
     vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        map<char,int> m;
+       
+        vector<int> hash(26, 0);
         vector<string> ans;
+        
         for(int i = 0; i < words2.size(); i++)
         {
             string s = words2[i];
-            map<char,int> m1;
+            vector<int> hashTemp(26, 0);
             for(int j = 0; j < s.size(); j++)
             {
-                m1[s[j]]++;
-                m[s[j]] = max(m1[s[j]], m[s[j]]);
+                hashTemp[(int)(s[j]-'a')]++;
+                hash[(int)(s[j]-'a')] = max(hashTemp[(int)(s[j]-'a')], hash[(int)(s[j]-'a')]);
             }
         }
         
         for(int i = 0; i < words1.size(); i++)
         {
             string s = words1[i];
-            map<char,int> m1;
+            vector<int> hashTemp(26, 0);;
             bool flag = true;
-            //cout<<s<<" "<<endl;
-            for(int j = 0; j < s.size(); j++)
-            {
-                m1[s[j]]++;
-            }
             
-            for(char c = 'a'; c <= 'z'; c++)
-            {
-                if(m1[c] < m[c]) {flag = false; break;}
-            }
+            for(int j = 0; j < s.size(); j++)
+                hashTemp[(int)(s[j]-'a')]++;
+           
+            for(int j = 0; j < 26; j++)
+                if(hashTemp[j] < hash[j]) {flag = false; break;}    
             
             if(flag) ans.push_back(words1[i]);
         }
