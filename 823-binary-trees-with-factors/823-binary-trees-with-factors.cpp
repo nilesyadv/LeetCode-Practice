74fcbe433b9691a8114cfa9b2510e81bac4e3947
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-08-10 03:23:24 +0530

    Time: 140 ms (37.23%), Space: 17.7 MB (5.85%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int numFactoredBinaryTrees(vector<int>& arr) {
+        
         sort(arr.begin(), arr.end());
         unordered_map<long long, long long> totalRoot, Divisor;
         
@@ -12,24 +13,11 @@ public:
             for(int j = i - 1; j >= 0; j--)
             {
                 if(arr[i] % arr[j] == 0 && Divisor[arr[i] / arr[j]])
-                {
-                    // if(arr[j] == arr[i] / arr[j])
-                    //if(arr[i]==20) cout<<totalRoot[arr[i]] <<" "<<totalRoot[arr[j]]<<" "<<totalRoot[arr[i]/arr[j]]<<endl;
-                    // {
-                        totalRoot[arr[i]] = (totalRoot[arr[i]] % M + ((totalRoot[arr[j]] % M) * (totalRoot[arr[i] / arr[j]] % M)) % M) % M;
-                    //}
-                    // else
-                    // {
-                    //     totalRoot[arr[i]] = (totalRoot[arr[i]] % M + ((((totalRoot[arr[j]] % M) * (totalRoot[arr[j]] % M)) % M) * 2) % M) % M;
-                    // }
-                }
+                    totalRoot[arr[i]] = (totalRoot[arr[i]] % M + ((totalRoot[arr[j]] % M) * (totalRoot[arr[i] / arr[j]] % M)) % M) % M;
             }
             totalRoot[arr[i]]++;
             ans = ((ans % M) + (totalRoot[arr[i]] % M)) % M;
         }
-        // cout<<endl;
-        // for(int i = 0; i < arr.size(); i++) cout << totalRoot[arr[i]] << " ";
-        // cout<<endl;
         
         return ans;
     }
