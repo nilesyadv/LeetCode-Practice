e62eae8ca5cf0cd646977c04646f10156d250297
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-08-10 03:25:43 +0530

    Time: 119 ms (48.92%), Space: 16.6 MB (25.54%) - LeetHub

--- 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -3,16 +3,15 @@ public:
     int numFactoredBinaryTrees(vector<int>& arr) {
         
         sort(arr.begin(), arr.end());
-        unordered_map<long long, long long> totalRoot, Divisor;
+        unordered_map<int, long long> totalRoot;
         
         long long ans = 0, M = 1e9 + 7;
         
         for(int i = 0; i < arr.size(); i++)
         {
-            Divisor[arr[i]]++;
             for(int j = i - 1; j >= 0; j--)
             {
-                if(arr[i] % arr[j] == 0 && Divisor[arr[i] / arr[j]])
+                if(arr[i] % arr[j] == 0)
                     totalRoot[arr[i]] = (totalRoot[arr[i]] % M + ((totalRoot[arr[j]] % M) * (totalRoot[arr[i] / arr[j]] % M)) % M) % M;
             }
             totalRoot[arr[i]]++;
