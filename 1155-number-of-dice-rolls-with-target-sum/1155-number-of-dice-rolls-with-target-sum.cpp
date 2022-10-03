739498d7936eff72c0d7557cb2b6de0da9ba81b7
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-03 22:42:10 +0530

    Time: 59 ms (41.64%), Space: 8.5 MB (29.00%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -2,8 +2,6 @@ class Solution {
     int helper(int n, int k, int target, vector<vector<int>> &dp){
         int totalWays = 0;
         if(target == 0 && n == 0) return 1;
-        if(target != 0 && n == 0) return 0;
-        if(target == 0 && n != 0) return 0;
         if(target < 0 || n < 0) return 0;
         if(dp[n][target] != -1) return dp[n][target];
         for(int i = 1; i <= k; i++){
