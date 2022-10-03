e7993003e22038349cdf132f6cbb2e64621e0ee1
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-03 22:38:51 +0530

    Time: 54 ms (46.67%), Space: 8.5 MB (29.00%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -1,53 +1,19 @@
 class Solution {
+    int helper(int n, int k, int target, vector<vector<int>> &dp){
+        int totalWays = 0;
+        if(target == 0 && n == 0) return 1;
+        if(target != 0 && n == 0) return 0;
+        if(target == 0 && n != 0) return 0;
+        if(target < 0 || n < 0) return 0;
+        if(dp[n][target] != -1) return dp[n][target];
+        for(int i = 1; i <= k; i++){
+            totalWays = ((totalWays % 1000000007) + (helper(n - 1, k, target - i, dp) % 1000000007)) % 1000000007;
+        }
+        return dp[n][target] = totalWays;
+    }
 public:
-    int numRollsToTarget(int d, int f, int target) {
-        return tabulation(d, f, target);
+    int numRollsToTarget(int n, int k, int target) {
+        vector<vector<int>> dp(n+1, vector<int>(target+1, -1));
+        return helper(n, k, target, dp);
     }
-    
-    int recursive(int d, int f, int target) {
-        if (d == 1) return target <= f ? 1 : 0;
-        
-        int res = 0;
-        for(int i = 1; i <= f && target - i > 0; ++i) {
-            res = (res + recursive(d - 1, f, target - i)) % 1000000007;
-        }
-        
-        return res;
-    }
-    
-    int memoization(int d, int f, int target) {
-        vector<vector<int>> mem(d + 1, vector<int>(target + 1, -1));
-        return solve(f, mem, d, target);
-    }
-    
-    int solve(int f, vector<vector<int>>& mem, int d, int target) {
-        if (d == 1) return target <= f ? 1 : 0;
-        if (mem[d][target] >= 0) return mem[d][target];
-        
-        mem[d][target] = 0;
-        for(int i = 1; i <= f && target - i > 0; ++i) {
-            mem[d][target] = (mem[d][target] + solve(f, mem, d-1, target-i)) % 1000000007;
-        }
-        
-        return mem[d][target];
-    }
-    
-    int tabulation(int d, int f, int target) {
-        vector<vector<int>> tab(d + 1, vector<int>(target + 1));
-        for(int i = 1; i < tab.size(); ++i) {
-            for(int j = 0; j < tab[0].size(); ++j) {
-                if (i == 1) {
-                    tab[i][j] = j <= f ? 1 : 0;
-                } else {
-                    tab[i][j] = 0;
-                    for(int k = 1; k <= f && j - k > 0; ++k) {
-                        tab[i][j] = (tab[i][j] + tab[i-1][j-k]) % 1000000007;
-                    }
-                }
-            }
-        }
-        
-        return tab[d][target];
-    }
-      
 };
