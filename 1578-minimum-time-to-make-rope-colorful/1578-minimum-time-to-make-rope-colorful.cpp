e87630e9b2ccc7cc39665a312e0bcb1e76b07cd0
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-03 20:25:18 +0530

    Time: 459 ms (9.80%), Space: 95.4 MB (50.05%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        vector<int> v(colors.size(), 0);
-        int i=0,j=1;
-        int ans=0,n=colors.size();
-        while(i < colors.size() && j < colors.size())
-        {
-            if(colors[i]!=colors[j]){
-                if(i<j) i=j+1;
-                else j=i+1;
+        int totalCost = neededTime[0], maxCost = neededTime[0], totalMaxCost = neededTime[0];
+        
+        for(int i = 1; i < neededTime.size(); i++){
+            if(colors[i] != colors[i-1]){
+                maxCost = neededTime[i];
+                totalCost += neededTime[i];
+                totalMaxCost += maxCost;
                 continue;
             }
-            if(neededTime[i]<=neededTime[j]){
-                ans+=neededTime[i];
-                v[i]=1;
-                while(i<n && (v[i]==1 || i==j)) {
-                    i++;
-                }
-            }
-            else{
-                ans+=neededTime[j];
-                v[j]=1;
-                while(j<n && (v[j]==1 || i==j)) {
-                    j++;
-                }
-            }
+            totalCost += neededTime[i];
+            totalMaxCost -= maxCost;
+            maxCost = max(maxCost, neededTime[i]);
+            totalMaxCost += maxCost;
         }
-        return ans;
+        return totalCost - totalMaxCost;
     }
 };
