74025005fe24b850dde8df29ac3e746a71ca683d
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-08 19:40:38 +0530

    Time: 301 ms (83.62%), Space: 16.3 MB (84.20%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -9,28 +9,19 @@ public:
         for(int i = 0; i < nums.size(); i++)
         {
             int j = i + 1, k = nums.size() - 1;
-            int sum = nums[i];
             bool flag = false;
-            //cout<<nums[i]<<endl;
+    
             while(j < k)
             {
-                sum = nums[i];
-                //cout<<nums[j]<<" "<<nums[k]<<endl;
-                sum += nums[j] + nums[k];
+                int sum = nums[i] + nums[j] + nums[k];
                 if(abs(sum - target < diff))
                 {
                     ans = sum;
                     diff = abs(sum - target);
                 }
-                //cout<<nums[j]<<" "<<nums[k]<<" ans->"<<ans<<endl;
-                if(sum > target)
-                {
-                    k--;
-                }
-                else if(sum < target) 
-                {
-                    j++;
-                }
+                
+                if(sum > target) k--;
+                else if(sum < target) j++;
                 else
                 {
                     flag = true;
