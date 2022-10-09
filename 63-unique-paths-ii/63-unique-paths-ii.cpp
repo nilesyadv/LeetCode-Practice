10a6dbaa754a2f1a7f0a495d3cf94a64131de4d5
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-09 20:53:22 +0530

    Time: 2 ms (88.41%), Space: 7.7 MB (68.06%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -5,34 +5,19 @@ public:
         int m = obstacleGrid.size(), n = obstacleGrid[0].size();
         vector<vector<int>> dp(m, vector<int>(n, 0));
         
-        if(obstacleGrid[0][0] == 1) dp[0][0] = 0;
-        else dp[0][0] = 1;
-    
-        for(int i = 1; i < n; i++)
-        {
-            if(obstacleGrid[0][i] == 1)
-                dp[0][i] = 0;
-            else dp[0][i] = dp[0][i-1];
-        }
+        if(obstacleGrid[0][0] != 1) dp[0][0] = 1;
         
-        for(int i = 1; i < m; i++)
+        for(int i = 0; i < m; i++)
         {
-            if(obstacleGrid[i][0] == 1)
-                dp[i][0] = 0;
-            else dp[i][0] = dp[i-1][0];
-        }
-        
-        
-        for(int i = 1; i < m; i++)
-        {
-            for(int j = 1; j < n; j++)
+            for(int j = 0; j < n; j++)
             {
                 if(obstacleGrid[i][j] == 1) 
                 {
                     dp[i][j] = 0;
                     continue;
                 }
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                if(i-1 >= 0 && j >= 0) dp[i][j] += dp[i - 1][j];
+                if(i >= 0 && j - 1 >= 0) dp[i][j] += dp[i][j - 1];
             }
         }
        
