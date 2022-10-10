a046e6ca45de40cc5bd69e18785a2977279acf7d
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-10 16:32:15 +0530

    Time: 4 ms (29.56%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -3,48 +3,15 @@ public:
     string breakPalindrome(string palindrome) {
         bool flag = false;
         int n = palindrome.size();
-        if(n % 2 == 0)
+        for(int i = 0; i < n/2; i++)
         {
-            for(int i = 0; i < n; i++)
+            if(palindrome[i] != 'a')
             {
-                if(palindrome[i] > 'a')
-                {
-                    palindrome[i] = 'a';
-                    flag = true;
-                    break;
-                }
-                else if(i == n-1)
-                {
-                    palindrome[i] += 1;
-                    flag = true;
-                    break;
-                }
+                palindrome[i] = 'a';
+                return palindrome;
             }
         }
-        else
-        {
-            for(int i = 0; i < n; i++)
-            {
-                if(i != n/2)
-                {
-                    //cout<<i<<" "<<n/2<<endl;
-                    if(palindrome[i] > 'a')
-                    {
-                        palindrome[i] = 'a';
-                        flag = true;
-                        break;
-                    }
-                    else if(i == n-1)
-                    {
-                        palindrome[i] += 1;
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-        }
-        
-        if(flag) return palindrome;
-        else return "";
+        palindrome[n-1] = 'b';
+        return n == 1 ? "" : palindrome;
     }
 };
