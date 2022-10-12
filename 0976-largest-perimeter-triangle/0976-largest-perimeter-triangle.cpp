818a4bce908b37e073df166f60dfc55dad1b2b55
Nilesh Kumar <75140705+nileshky1@users.noreply.github.com>
2022-10-12 11:43:07 +0530

    Time: 114 ms (5.51%), Space: 21.9 MB (71.51%) - LeetHub

--- 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
+++ 0976-largest-perimeter-triangle/0976-largest-perimeter-triangle.cpp
@@ -4,8 +4,7 @@ public:
         sort(nums.rbegin(), nums.rend());
         for(int i = 0; i < nums.size() - 2; i++)
         {
-            if((nums[i] + nums[i+1] > nums[i+2]) && (nums[i] + nums[i+2] > nums[i+1]) 
-               && (nums[i+2] + nums[i+1] > nums[i])) return nums[i] + nums[i+1] + nums[i+2];
+            if(nums[i] < nums[i+1] + nums[i+2]) return nums[i] + nums[i+1] + nums[i+2];
         }
         return 0;
     }
