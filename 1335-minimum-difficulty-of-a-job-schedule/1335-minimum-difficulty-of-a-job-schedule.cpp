b12ee4e7347e9028821db9d9f8f57084bb99b4cc
Nilesh Kumar <nileshky7@gmail.com>
2022-10-16 19:30:13 +0530

    Time: 81 ms (55.73%), Space: 7.9 MB (30.04%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -2,19 +2,15 @@ class Solution {
     int helper(int ind, int n, vector<vector<int>> &dp, vector<int> &jobDifficulty, int d)
     {
         if(ind >= n) return 0;
-        if(n - ind < d) return -1;
         if(dp[ind][d] != -1) return dp[ind][d];
-        //cout<<ind<<" "<<d<<endl;
         
         int ans = 1000000000, maxi = jobDifficulty[ind];
         
         if(d == 1)
         {
             for(int i = ind; i < n; i++)
-            {
                 maxi = max(maxi, jobDifficulty[i]);
-                //ans = min(ans, maxi + helper(i+1, n, dp, jobDifficulty, d - 1));
-            }
+        
             ans = maxi;
         }
         else
@@ -32,7 +28,7 @@ public:
     int minDifficulty(vector<int>& jobDifficulty, int d) {
         int n = jobDifficulty.size();
         vector<vector<int>> dp(n + 1, vector<int> (d + 1, -1));
-        //if(n < d) return -1;
+        if(n < d) return -1;
         return helper(0, n, dp, jobDifficulty, d);
     }
 };
